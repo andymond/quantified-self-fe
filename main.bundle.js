fa77ba5a7741d606415ffe4bc0fed71b5b2c3b11
Andrew Dymond <andy.dymond@gmail.com>
2018-05-08 15:51:40 -0600

    Run rails build

--- main.bundle.js
+++ main.bundle.js
@@ -414,8 +414,8 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    return "https://calm-harbor-70156.herokuapp.com/"; //node backend host
-	    // return `https://murmuring-earth-90329.herokuapp.com` //rails backend host
+	    // return `https://calm-harbor-70156.herokuapp.com/` //node backend host
+	    return "https://murmuring-earth-90329.herokuapp.com"; //rails backend host
 	  }
 	};
 
